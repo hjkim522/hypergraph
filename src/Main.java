d78283e37685f1d68801c93779432ce49ec15186
KimHyunjun <njurn@naver.com>
2015-04-15 00:51:19 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( "db/graphDb" );
+        registerShutdownHook(graphDb);
+        try (Transaction tx = graphDb.beginTx()) {
+            Node node = graphDb.createNode();
+            node.setProperty("Hello", "world");
+            Node node2 = graphDb.createNode();
+            node2.setProperty("Hello", "world");
+            tx.success();
+        }
+
+        graphDb.shutdown();
+    }
+
+    private static void registerShutdownHook(final GraphDatabaseService graphDb)
+    {
+        // Registers a shutdown hook for the Neo4j instance so that it
+        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+        // running application).
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                graphDb.shutdown();
+            }
+        });
+    }
+}
