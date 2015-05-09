c01aef226767400ce1dcf11f57ec0228052cf34f
KimHyunjun <njurn@naver.com>
2015-05-09 13:03:33 +0900

    check dup

--- src/hypergraph/common/Hyperedge.java
+++ src/hypergraph/common/Hyperedge.java
@@ -113,8 +113,6 @@ public class Hyperedge {
             e.target = new HashSet<>();
             e.hypernode = h;
 
-            Log.debug("hypernode " + h.getId());
-
             // make target set
             Iterable<Relationship> rels = h.getRelationships(Direction.OUTGOING, Const.REL_TO_TARGET);
             for (Relationship rel : rels) {
