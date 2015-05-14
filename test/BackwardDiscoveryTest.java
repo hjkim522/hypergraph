0e9489599e5b9ccc9484d513ee1730fb99a3197e
Kim Hyunjun <hjkim@dbserver.kaist.ac.kr>
2015-05-14 14:00:42 +0900

    use bf

--- test/BackwardDiscoveryTest.java
+++ test/BackwardDiscoveryTest.java
@@ -4,7 +4,7 @@ import hypergraph.data.Importer;
 import hypergraph.data.SimpleImporter;
 import hypergraph.mss.*;
 import hypergraph.util.Log;
-import org.junit.Test;
+import org.junit.*;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
@@ -15,14 +15,25 @@ import static org.junit.Assert.*;
  * Created by Hyunun on 2015-05-14.
  */
 public class BackwardDiscoveryTest {
-    @Test
-    public void testIndexedBackwardDiscovery() throws Exception {
+    GraphDatabaseService graphDb;
+
+    @Before
+    public void openDatabase() {
+        graphDb = HypergraphDatabase.open("db/kegg");
+    }
+
+    @After
+    public void closeDatabase() {
+        HypergraphDatabase.close();
     }
 
     @Test
-    public void testBare() throws Exception {
-        GraphDatabaseService graphDb = HypergraphDatabase.open("db/kegg");
+    public void testIndexedBackwardDiscovery() throws Exception {
+        
+    }
 
+    @Test
+    public void testWithoutReconstruction() throws Exception {
         try (Transaction tx = graphDb.beginTx()) {
             Node t = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, "cpd:C05345");
 
@@ -32,7 +43,5 @@ public class BackwardDiscoveryTest {
             Log.debug("size " + mss.size());
             Log.debug("card " + mss.cardinality());
         }
-
-        HypergraphDatabase.close();
     }
 }
