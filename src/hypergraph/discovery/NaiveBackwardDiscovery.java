0eefdf5f249b9c384edb2f3d516d789bfe986ed3
KimHyunjun <njurn@naver.com>
2015-05-15 23:07:02 +0900

    impl naive bd

--- src/hypergraph/discovery/NaiveBackwardDiscovery.java
+++ src/hypergraph/discovery/NaiveBackwardDiscovery.java
@@ -3,6 +3,7 @@ package hypergraph.discovery;
 import hypergraph.Application;
 import hypergraph.common.Const;
 import hypergraph.common.HypergraphDatabase;
+import hypergraph.util.Log;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -19,6 +20,10 @@ public class NaiveBackwardDiscovery implements BackwardDiscovery {
     private GraphDatabaseService graphDb;
     private Set<Long> visited;
 
+    // to enumerate backward star
+    Stack<Node> branchingNode = new Stack<>();
+    Map<Long, Integer> nextEdge = new HashMap<>();
+
     public NaiveBackwardDiscovery() {
         graphDb = HypergraphDatabase.getGraphDatabase();
         visited = new HashSet<>();
@@ -28,43 +33,104 @@ public class NaiveBackwardDiscovery implements BackwardDiscovery {
     public Set<Long> find(Node t) {
         Set<Node> targets = new HashSet<>();
         targets.add(t);
-        Set<Node> sourceSet = findWithTraversal(targets);
+
+        findOpt(targets);
+
+//        Set<Node> sourceSet = findWithTraversal(targets);
+//        printNodes(sourceSet);
+//
+//        printNodes(findWithTraversal(targets));
+//
         Set<Long> result = new HashSet<Long>();
-        for (Node s : sourceSet) {
-            result.add(s.getId());
-        }
+//        for (Node s : sourceSet) {
+//            result.add(s.getId());
+//        }
+
         return result;
+    }
+
+    public Set<Long> findOpt(Set<Node> targets) {
+
+        do {
+            visited = new HashSet<>();
+            Set<Node> sources = findWithTraversal(targets);
+            printNodes(sources);
+
+            // manage branch
+            while (!branchingNode.empty()) {
+                Node b = branchingNode.peek();
+                int inDegree = b.getDegree(Const.REL_TO_TARGET, Direction.INCOMING);
+                int edgeIndex = nextEdge.getOrDefault(b.getId(), 0);
+
+                int name = (int) b.getProperty(Const.PROP_UNIQUE);
+                Log.debug("branching node " + name);
+
+                if (!b.hasLabel(Const.LABEL_STARTABLE) && edgeIndex == 0)
+                    edgeIndex = 1;
+
+                if (inDegree == edgeIndex) {
+                    branchingNode.pop();
+                } else if (inDegree > edgeIndex) {
+                    nextEdge.put(b.getId(), edgeIndex + 1);
+                    break;
+                }
+            }
+        } while (!branchingNode.empty());
+
+        return null;
+    }
+
+    private void printNodes(Set<Node> nodes) {
+        System.out.print("{");
+        for (Node v : nodes) {
+            int name = (int) v.getProperty(Const.PROP_UNIQUE);
+            System.out.print(name + ", ");
+        }
+        System.out.println("}");
     }
 
     private Set<Node> findWithTraversal(Set<Node> targets) {
         Queue<Node> queue = new LinkedList<Node>();
-        Stack<Node> stack = new Stack<>();
-        Map<Long, Integer> choice = new HashMap<>();
+        Set<Node> result = new HashSet<>();
 
         for (Node t : targets) {
             setVisited(t);
             queue.add(t);
+            if (t.hasLabel(Const.LABEL_STARTABLE))
+                result.add(t);
         }
 
         while (!queue.isEmpty()) {
             Node v = queue.poll();
             Node selectedHyperedge = null;
+
             int inDegree = v.getDegree(Const.REL_TO_TARGET, Direction.INCOMING);
-            int edgeSelect = 1; //XXX: temp
-            int edgeCount = 0;
+            if (inDegree == 0)
+                continue;
+
+            int edgeIndex = nextEdge.getOrDefault(v.getId(), 0);
+
+            // if startable, then can choose stop traversing!
+            if (v.hasLabel(Const.LABEL_STARTABLE) && edgeIndex == 0) {
+                if (inDegree > 0 && !branchingNode.contains(v)) {
+                    branchingNode.push(v);
+                }
+                continue;
+            }
+
+            if (!v.hasLabel(Const.LABEL_STARTABLE) && edgeIndex == 0)
+                edgeIndex = 1;
 
             // selected a hyperedge from backward star
+            int edgeCount = 0;
             Iterable<Relationship> toTargets = v.getRelationships(Direction.INCOMING, Const.REL_TO_TARGET);
             for (Relationship toTarget : toTargets) {
                 edgeCount++;
-                if (edgeCount == edgeSelect) {
+                if (edgeCount == edgeIndex) {
                     selectedHyperedge = toTarget.getStartNode();
                     break;
                 }
             }
-
-            if (selectedHyperedge == null) // or visited
-                continue;
 
             // insert sources into queue
             Iterable<Relationship> fromSources = selectedHyperedge.getRelationships(Direction.INCOMING, Const.REL_FROM_SOURCE);
@@ -73,23 +139,31 @@ public class NaiveBackwardDiscovery implements BackwardDiscovery {
                 if (!isVisited(s)) {
                     setVisited(s);
                     queue.add(s);
+                    if (s.hasLabel(Const.LABEL_STARTABLE))
+                        result.add(s);
                 }
+            }
+
+            // edge remains
+//            if (edgeCount < inDegree && !branchingNode.contains(v)) {
+            if (inDegree > 1 && !branchingNode.contains(v)) {
+                branchingNode.push(v);
             }
         }
 
-        return null;//result;
+        return result;
     }
 
-    // can find a minimal set
-    // XXX: temporarily save
-    private Set<Node> findAnyTraversal(Set<Node> targets) {
+    // can find any minimal set
+    private Set<Node> findAny(Set<Node> targets) {
         Queue<Node> queue = new LinkedList<Node>();
-        Stack<Node> stack = new Stack<>();
-        Map<Long, Integer> choice = new HashMap<>();
+        Set<Node> result = new HashSet<>();
 
         for (Node t : targets) {
             setVisited(t);
             queue.add(t);
+            if (t.hasLabel(Const.LABEL_STARTABLE))
+                result.add(t);
         }
 
         while (!queue.isEmpty()) {
@@ -119,11 +193,13 @@ public class NaiveBackwardDiscovery implements BackwardDiscovery {
                 if (!isVisited(s)) {
                     setVisited(s);
                     queue.add(s);
+                    if (s.hasLabel(Const.LABEL_STARTABLE))
+                        result.add(s);
                 }
             }
         }
 
-        return null;//result;
+        return result;
     }
 
     private void setVisited(Node node) {
--- test/BackwardDiscoveryTest.java
+++ test/BackwardDiscoveryTest.java
@@ -4,7 +4,9 @@ import hypergraph.data.Importer;
 import hypergraph.data.SimpleImporter;
 import hypergraph.discovery.BackwardDiscovery;
 import hypergraph.discovery.IndexedBackwardDiscovery;
+import hypergraph.discovery.NaiveBackwardDiscovery;
 import hypergraph.mss.*;
+import hypergraph.traversal.BackwardTraversal;
 import hypergraph.util.Log;
 import org.junit.*;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -16,23 +18,13 @@ import java.util.Set;
 import static org.junit.Assert.*;
 
 /**
- * Created by Hyunun on 2015-05-14.
+ * Created by Hyunjun on 2015-05-14.
  */
 public class BackwardDiscoveryTest {
-    GraphDatabaseService graphDb;
-
-    @Before
-    public void openDatabase() {
-        graphDb = HypergraphDatabase.open("db/kegg");
-    }
-
-    @After
-    public void closeDatabase() {
-        HypergraphDatabase.close();
-    }
 
     @Test
     public void testIndexedBackwardDiscovery() throws Exception {
+        GraphDatabaseService graphDb = HypergraphDatabase.open("db/kegg");
         try (Transaction tx = graphDb.beginTx()) {
             Node t = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, "hsa:5161");
 
@@ -50,18 +42,47 @@ public class BackwardDiscoveryTest {
                 System.out.println("}");
             }
         }
+        HypergraphDatabase.close();
     }
 
     @Test
-    public void testWithoutReconstruction() throws Exception {
-        try (Transaction tx = graphDb.beginTx()) {
-            Node t = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, "cpd:C05345");
+    public void testNaiveBackwardDiscovery() throws Exception {
+        GraphDatabaseService graphDb = HypergraphDatabase.init("db/hypergraph");
+        SimpleImporter importer = new SimpleImporter("input/naive-backward-discovery-test.txt");
+//        SimpleImporter importer = new SimpleImporter("input/example-2.txt");
+        importer.run();
 
-            String mssStr = (String) t.getProperty(Const.PROP_MSS);
-            MinimalSourceSet mss = MinimalSourceSet.valueOf(mssStr);
-            Log.debug(mssStr);
-            Log.debug("size " + mss.size());
-            Log.debug("card " + mss.cardinality());
+        try (Transaction tx = graphDb.beginTx()) {
+            Node t = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, 0);
+
+            NaiveBackwardDiscovery discovery = new NaiveBackwardDiscovery();
+            Set<Long> source = discovery.find(t);
+
+            // source to print
+//            System.out.print("{");
+//            for (Long sid : source) {
+//                Node s = graphDb.getNodeById(sid);
+//                int name = (int) s.getProperty(Const.PROP_UNIQUE);
+//                System.out.print(name + ", ");
+//            }
+//            System.out.println("}");
         }
+
+        HypergraphDatabase.close();
     }
+
+    @Test
+    public void testNaiveWithBackawrdTraversal() throws Exception {
+        GraphDatabaseService graphDb = HypergraphDatabase.init("db/hypergraph");
+        SimpleImporter importer = new SimpleImporter("input/naive-backward-discovery-test.txt");
+        importer.run();
+
+        try (Transaction tx = graphDb.beginTx()) {
+            Node t = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, 5);
+            BackwardTraversal bt = new BackwardTraversal();
+        }
+
+        HypergraphDatabase.close();
+    }
+
 }
