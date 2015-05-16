01fb550c717045558a124951039e074e8255e3b2
KimHyunjun <njurn@naver.com>
2015-05-17 01:51:53 +0900

    bf

--- src/hypergraph/discovery/BruteForceBackwardDiscovery.java
+++ src/hypergraph/discovery/BruteForceBackwardDiscovery.java
@@ -25,7 +25,14 @@ public class BruteForceBackwardDiscovery implements BackwardDiscovery {
         traversal.traverse(target);
 
         // Test all possible cases
-        
+        // 1 to 2^k - 1, k = |sources|
+        int k = sources.size();
+        if (k > 64) return null; //XXX: naive impl
+
+        long n = (1 << k) - 1;
+        for (long i = 1; i <= n; i++) {
+            //TODO:
+        }
 
         return null;
     }
