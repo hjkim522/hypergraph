bfed2b8237d8ad030dd82ab15e89f152881268f2
Kim Hyunjun <hjkim@dbserver.kaist.ac.kr>
2015-05-23 01:53:47 +0900

    coda change

--- src/hypergraph/data/CodaImporter.java
+++ src/hypergraph/data/CodaImporter.java
@@ -4,10 +4,7 @@ import hypergraph.common.Const;
 import hypergraph.common.Hyperedge;
 import hypergraph.common.HypergraphDatabase;
 import hypergraph.util.Log;
-import org.neo4j.graphdb.DynamicLabel;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 
 import javax.sound.sampled.Line;
 import java.io.BufferedReader;
@@ -29,120 +26,66 @@ import java.util.Set;
  * Created by Hyunjun on 2015-05-18.
  */
 public class CodaImporter implements Importer {
-
-    private class CodaEntity {
-        String id;
-        String originId;
-        String name;
-        String synonym;
-
-        public CodaEntity(String s) {
-            String[] token = s.split("\t");
-            id = token[0];
-            originId = token[1];
-            name = token[2];
-            synonym = (token.length > 3) ? token[3] : "";
-        }
-
-        public String toString() {
-            return id + "(" + name + ") " + synonym;
-        }
-    }
-
-    private class CodaSide {
-        String id;
-        String[] tags;
-
-        public CodaSide(String s) {
-            String data[] = s.split(" ");
-            id = data[0];
-            //tags = data[1];//.split("<>"); //TODO:complex
-        }
-    }
-
     private class CodaRule {
-
+        //TODO:
     }
 
     private GraphDatabaseService graphDb;
     private int countEntity;
-    private int countRule;
-    private Map<String, String> ruleTypes;
+    private int countRelationship;
+
+    private Map<String, String> relationMap;
+    private Map<String, String> geneMap;
 
     public CodaImporter() {
         graphDb = HypergraphDatabase.getGraphDatabase();
         countEntity = 0;
-        countRule = 0;
+        countRelationship = 0;
     }
 
     @Override
     public void run() {
-        constructRuleType(new File("input/coda/BISL_Ontology/relation(RE).txt"));
-        importEntityFile(new File("input/coda/BISL_Ontology/gene(GE)_HomoSapiens.txt"), "Gene");
-        importEntityFile(new File("input/coda/BISL_Ontology/disease(DS).txt"), "Disease");
-        importEntityFile(new File("input/coda/BISL_Ontology/metabolite(MB).txt"), "Metabolite");
-        importRuleFile(new File("input/coda/FinalNetwork/CODA2_Gene_Disease_Network.txt"));
+        relationMap = constructMap(new File("input/coda/BISL_Ontology/relation(RE).txt"), 1);
+//        geneMap = constructMap(new File("input/coda/BISL_Ontology/gene(GE)_HomoSapiens.txt"), 2);
+
+//        importRuleFile(new File("input/coda/FinalNetwork/CODA2_Gene_Disease_Network.txt"));
         importRuleFile(new File("input/coda/FinalNetwork/CODA2_Inter_Cell_Network.txt"));
 //        importRuleFile(new File("input/coda/FinalNetwork/CODA2_Intra_Cell_Network.txt"));
-//        importRuleFile(new File("input/coda/FinalNetwork/kegg.txt"));
-        importDrugAndInteraction(new File("input/coda/drug_target_interaction_alldrugs.txt"));
+        importRuleFile(new File("input/coda/FinalNetwork/kegg.txt"));
+//        importDrugAndInteraction(new File("input/coda/drug_target_interaction_alldrugs.txt"));
 
         Log.info("CodaImporter DONE");
         Log.info("countEntity: " + countEntity);
-        Log.info("countRule: " + countRule);
+        Log.info("countRelationship: " + countRelationship);
     }
 
-    private void constructRuleType(File file) {
-        ruleTypes = new HashMap<>();
+    private Map<String, String> constructMap(File file, int nameColumn) {
+        Map<String, String> result = new HashMap<>();
         try (FileReader fr = new FileReader(file)) {
             BufferedReader br = new BufferedReader(fr);
             String s = br.readLine(); // skip first line
             while ((s = br.readLine()) != null) {
                 if (s.startsWith("<DB END>")) break;
-                CodaEntity entity = new CodaEntity(s);
-                ruleTypes.put(entity.id, entity.originId);
+                String[] token = s.split("\t");
+                result.put(token[0], token[nameColumn]);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void importEntityFile(File file, String label) {
-        try (Transaction tx = graphDb.beginTx()) {
-            try (FileReader fr = new FileReader(file)) {
-                BufferedReader br = new BufferedReader(fr);
-                String s = br.readLine(); // skip first line
-                while ((s = br.readLine()) != null) {
-                    importEntity(s, label);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            tx.success();
-        }
-    }
-
-    private void importEntity(String row, String label) {
-        CodaEntity entity = new CodaEntity(row);
-
-        if (graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, entity.id) != null)
-            return;
-
-        Node node = graphDb.createNode(Const.LABEL_NODE);
-        node.setProperty(Const.PROP_UNIQUE, entity.id);
-        node.setProperty("name", entity.name);
-        node.setProperty("synonym", entity.synonym);
-        node.addLabel(DynamicLabel.label(label));
-        countEntity++;
+        return result;
     }
 
     private void importRuleFile(File file) {
+        int count = 0;
         try (Transaction tx = graphDb.beginTx()) {
             try (FileReader fr = new FileReader(file)) {
                 BufferedReader br = new BufferedReader(fr);
                 String s;
                 while ((s = br.readLine()) != null) {
+                    Log.debug(s);
                     importRule(s);
+                    count++;
+//                    if (count > 100000) return;
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -166,60 +109,50 @@ public class CodaImporter implements Importer {
         left = left.substring(5, left.length() - 1);
         right = right.substring(6, right.length() - 1);
 
-//        Log.debug(left);
-//        Log.debug(right);
+        // parse each side
+        Set<Node> leftSide = parseSide(left);
+        Set<Node> rightSide = parseSide(right);
 
-        Set<CodaSide> source = parseSides(left);
-        Set<CodaSide> target = parseSides(right);
-
-        if (source == null || target == null)
+        if (leftSide == null || rightSide == null)
             return;
 
-        Hyperedge h = new Hyperedge();
-
-        for (CodaSide s : source) {
-            Node node = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, s.id);
-            if (node == null) {
-                Log.error("no source entry : " + s.id);
-                return;
-            }
-            h.addSource(node);
-        }
-
-        for (CodaSide t : target) {
-            Node node = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, t.id);
-            if (node == null) {
-                Log.error("no target entry : " + t.id);
-                return;
-            }
-            h.addTarget(node);
-        }
-
+        Hyperedge h = new Hyperedge(leftSide, rightSide);
         if (!isDuplicated(h)) {
             h.save(graphDb);
-            Node hypernode = h.getHypernode();
-            if (hypernode != null) {
-                hypernode.setProperty("type", rel);
-                hypernode.setProperty("name", ruleTypes.get(rel));
-                countRule++;
+            if (h.getHypernode() != null) {
+                h.getHypernode().setProperty("type", rel);
+                h.getHypernode().setProperty("name", relationMap.get(rel));
+                countRelationship++;
             }
         }
     }
 
-    private Set<CodaSide> parseSides(String s) {
+    private Set<Node> parseSide(String s) {
+        Set<Node> result = new HashSet<>();
+
         //TODO: handle complex
         if (s.startsWith("complex"))
             return null;
 
-        Set<CodaSide> result = new HashSet<>();
         String[] entries = s.split(",");
         for (String entry : entries) {
             if (entry.startsWith(" "))
                 entry = entry.substring(1, entry.length());
             if (entry.startsWith("<"))
                 continue;
-            CodaSide codaRelSide = new CodaSide(entry);
-            result.add(codaRelSide);
+
+            // find or create a node
+            Node node = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, entry);
+            if (node == null) {
+                node = graphDb.createNode(Const.LABEL_NODE);
+                node.setProperty(Const.PROP_UNIQUE, entry);
+
+                Label label = getLabel(entry);
+                if (label != null) node.addLabel(label);
+
+                countEntity++;
+            }
+            result.add(node);
         }
         return result;
     }
@@ -234,60 +167,10 @@ public class CodaImporter implements Importer {
         return false;
     }
 
-    private void importDrugAndInteraction(File file) {
-        try (Transaction tx = graphDb.beginTx()) {
-            try (FileReader fr = new FileReader(file)) {
-                BufferedReader br = new BufferedReader(fr);
-                String s = br.readLine(); // skip line
-                while ((s = br.readLine()) != null) {
-                    importDrug(s);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            tx.success();
-        }
-    }
-
-    private void importDrug(String s) {
-        String[] data = s.split("\t");
-        String name = data[0];
-        String up = data[1];
-        String down = data[2];
-
-        Node node = graphDb.createNode(Const.LABEL_NODE);
-        node.addLabel(Const.LABEL_STARTABLE);
-        node.addLabel(DynamicLabel.label("Drug"));
-        node.setProperty(Const.PROP_UNIQUE, name);
-        node.setProperty("name", name);
-
-        if (!up.startsWith("null"))
-            importDrugInteraction(node, up);
-        if (!down.startsWith("null"))
-            importDrugInteraction(node, down);
-    }
-
-    private void importDrugInteraction(Node node, String targetSeq) {
-        Set<String> targetIds = new HashSet<>();
-//        Log.debug(targetSeq);
-        String[] targets = targetSeq.split("\\|");
-        for (String target : targets) {
-//            Log.debug(target);
-            String id = target.split(" ")[0];
-//            Log.debug(id);
-            targetIds.add(id);
-        }
-
-        Hyperedge h = new Hyperedge();
-        h.addSource(node);
-
-        for (String target : targetIds) {
-            Node t = graphDb.findNode(Const.LABEL_NODE, Const.PROP_UNIQUE, target);
-            h.addTarget(t);
-        }
-
-        if (!isDuplicated(h)) {
-            h.save(graphDb);
-        }
+    private Label getLabel(String entry) {
+        if (entry.startsWith("GE")) return DynamicLabel.label("Gene");
+        else if (entry.startsWith("MB")) return DynamicLabel.label("Metabolite");
+        else if (entry.startsWith("DS")) return DynamicLabel.label("Disease");
+        return null;
     }
 }
