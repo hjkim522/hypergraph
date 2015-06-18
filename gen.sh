b24543ef83879a5dc89ef50f652d2f9e12f7b705
Hyunjun Kim <njurn@njurn-mbp.local>
2015-06-19 04:51:02 +0900

    add gen.sh

---
+++ gen.sh
@@ -0,0 +1,46 @@
+#python input/gen.py 7500 7500 375 input/vs07-05.txt
+#python input/gen.py 7500 7500 750 input/vs07-10.txt
+#python input/gen.py 7500 7500 1125 input/vs07-15.txt
+#python input/gen.py 7500 7500 1500 input/vs07-20.txt
+#python input/gen.py 7500 7500 1875 input/vs07-25.txt
+#python input/gen.py 7500 7500 2250 input/vs07-30.txt
+#python input/gen.py 7500 7500 2625 input/vs07-35.txt
+#python input/gen.py 2500 2500 125 input/vs02-05.txt
+#python input/gen.py 2500 2500 250 input/vs02-10.txt
+#python input/gen.py 2500 2500 375 input/vs02-15.txt
+#python input/gen.py 2500 2500 500 input/vs02-20.txt
+#python input/gen.py 2500 2500 625 input/vs02-25.txt
+#python input/gen.py 2500 2500 750 input/vs02-30.txt
+#python input/gen.py 2500 2500 875 input/vs02-35.txt
+# python input/gen.py 10000 10000 500 input/vs10-05.txt
+# python input/gen.py 10000 10000 1000 input/vs10-10.txt
+# python input/gen.py 10000 10000 1500 input/vs10-15.txt
+# python input/gen.py 10000 10000 2000 input/vs10-20.txt
+# python input/gen.py 10000 10000 2500 input/vs10-25.txt
+# python input/gen.py 10000 10000 3000 input/vs10-30.txt
+# python input/gen.py 10000 10000 3500 input/vs10-35.txt
+
+python input/gen.py 2500 2500 250 input/e25-100.txt
+python input/gen.py 2500 3125 250 input/e25-125.txt
+python input/gen.py 2500 3750 250 input/e25-150.txt
+python input/gen.py 2500 4375 250 input/e25-175.txt
+python input/gen.py 2500 5000 250 input/e25-200.txt
+
+python input/gen.py 5000 5000 500 input/e50-100.txt
+python input/gen.py 5000 6250 500 input/e50-125.txt
+python input/gen.py 5000 7500 500 input/e50-150.txt
+python input/gen.py 5000 8750 500 input/e50-175.txt
+python input/gen.py 5000 10000 500 input/e50-200.txt
+
+python input/gen.py 7500 7500 750 input/e75-100.txt
+python input/gen.py 7500 9375 750 input/e75-125.txt
+python input/gen.py 7500 11250 750 input/e75-150.txt
+python input/gen.py 7500 13125 750 input/e75-175.txt
+python input/gen.py 7500 15000 750 input/e75-200.txt
+
+python input/gen.py 10000 10000 1000 input/e100-100.txt
+python input/gen.py 10000 12500 1000 input/e100-125.txt
+python input/gen.py 10000 15000 1000 input/e100-150.txt
+python input/gen.py 10000 17500 1000 input/e100-175.txt
+python input/gen.py 10000 20000 1000 input/e100-200.txt
+
