f66538122fea355d6e38ea26a9c96f451942f0bd
Kim Hyunjun <hjkim@dbserver.kaist.ac.kr>
2015-07-31 23:15:26 +0900

    remove unnecessary

--- src/hypergraph/mss/RecursiveFinder.java
+++
@@ -1,7 +0,0 @@
-package hypergraph.mss;
-
-/**
- * Created by Hyunjun on 2015-07-23.
- */
-public class RecursiveFinder {
-}
---
+++ src/hypergraph/mss/TargetableFinder.java
@@ -0,0 +1,8 @@
+package hypergraph.mss;
+
+/**
+ * Created by Hyunjun on 2015-07-31.
+ */
+public class TargetableFinder extends FastDecompositionFinder {
+
+}
